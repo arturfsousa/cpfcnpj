dbf6c314d6caf573f5c18f606275009a13f9bc12
arturfelipe <arturfelipe.sousa@gmail.com>
2012-09-16 12:49:31 -0300

    Finalizando classe Cnpj e o método gerar_randomico

--- cnpj.py
+++ cnpj.py
@@ -1,17 +1,63 @@
 # coding: utf-8
 
+import re
+import random
+
 class Cnpj(object):
 
     def __init__(self, num):
-        self.num = num
+        self._num = num
+        self._validate()
+
+    def _validate(self):
+        # Remove non-digits chars
+        self._num = filter(lambda c: c.isdigit(), self._num)
+
+        if len(self._num) != 14:
+            raise ValueError('Cnpj deve possuir 14 digitos')
+
+        if self.calculate_dv(self._num[:12]) != self._num[-2:]:
+            raise ValueError('Cnpj inválido')
+
+    @classmethod
+    def calculate_dv(cls, num):
+        pesos = (
+            (5, 4, 3, 2, 9, 8, 7, 6, 5, 4, 3, 2),
+            (6, 5, 4, 3, 2, 9, 8, 7, 6, 5, 4, 3, 2)
+        )
+        num_sem_dv = [int(i) for i in num]
+
+        tabela = zip(num_sem_dv, pesos[0])
+        soma = sum([c1*c2 for c1,c2 in tabela])
+        mod = soma % 11
+        dv1 = 0 if mod < 2 else 11 - mod
+
+        tabela = zip(num_sem_dv + [int(dv1)], pesos[1])
+        soma = sum([c1*c2 for c1,c2 in tabela])
+        mod = soma % 11
+        dv2 = 0 if mod < 2 else 11 - mod
+
+        return str(dv1)+str(dv2)
+
+    @classmethod
+    def generate_random(cls, total):
+        """Generates arbitrary distinct cnpj numbers"""
+        cnpjs = {}
+        while len(cnpjs) < total:
+            inscricao = ''.join([str(random.randrange(9)) for d in range(8)])
+            cd_matriz = '0001'
+            dv = cls.calculate_dv(inscricao+cd_matriz)
+            cnpj = inscricao+cd_matriz+dv
+            cnpjs[cnpj] = Cnpj(cnpj)
+        return cnpjs.values()
 
     def format(self):
         str_format = '{0}.{1}.{2}/{3}-{4}'
-        return str_format.format(self.num[:2], self.num[2:5], self.num[5:8],
-                                 self.num[8:12], self.num[12:])
+        return str_format.format(self._num[:2], self._num[2:5], self._num[5:8],
+                                 self._num[8:12], self._num[12:])
 
-    def validate(self):
-        return True
+    def unformat(self):
+        return self._num
 
     def __repr__(self):
         return self.format()
--- tests_doctest.rst
+++ tests_doctest.rst
@@ -3,9 +3,43 @@ Testing CpfCnpj
 ===============
 
   >>> from cnpj import Cnpj
+  >>> Cnpj('127818212312311')
+  Traceback (most recent call last):
+    ...
+  ValueError: Cnpj deve possuir 14 digitos
+
+  >>> Cnpj('123456789123')
+  Traceback (most recent call last):
+    ...
+  ValueError: Cnpj deve possuir 14 digitos
+
+  >>> Cnpj('1234.678912a11')
+  Traceback (most recent call last):
+    ...
+  ValueError: Cnpj deve possuir 14 digitos
+
+  >>> Cnpj('87552587000220')
+  Traceback (most recent call last):
+    ...
+  ValueError: Cnpj inválido
+
   >>> cnpj = Cnpj('87552587000120')
   >>> cnpj
   87.552.587/0001-20
 
-  >>> cnpj.validate()
+  >>> cnpj.format()
+  '87.552.587/0001-20'
+
+  >>> cnpj.unformat()
+  '87552587000120'
+
+  >>> print cnpj
+  87.552.587/0001-20
+
+  >>> cnpjs = Cnpj.generate_random(100)
+  >>> all([isinstance(cnpj, Cnpj) for cnpj in cnpjs])
   True
+
+  >>> len({cnpj.format() for cnpj in cnpjs})
+  100
+
